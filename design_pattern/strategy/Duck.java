fe28f835c0493c5db60949f0c07a9eb67cf76c1f
Murphy Han <11711416@mail.sustech.edu.cn>
2019-09-15 15:36:15 +0800

    first commit

--- design_pattern/strategy/Duck.java
+++ design_pattern/strategy/Duck.java
@@ -1,2 +1,23 @@
 
 
+public abstract class Duck{
+    private FlyBehavior flyBehavior;
+    private QuackBehavior quackBehavior;
+    public Duck(){
+
+    }
+    public abstract void display();
+
+    public void performFly(){
+        flyBehavior.fly();
+    }
+
+    public void performQuack(){
+        quackBehavior.quack();
+    }
+
+    public void swim(){
+        System.out.println("All ducks can swim");
+    }
+
+}
---
+++ design_pattern/strategy/FlyWithWings.java
@@ -0,0 +1,6 @@
+class FlyWithWings implements FlyBehavior{
+    @Override
+    public void fly(){
+        System.out.println("I am flying!");
+    }
+}
